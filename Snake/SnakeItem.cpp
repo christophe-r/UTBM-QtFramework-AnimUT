c04954e8fddda18bedd59f2dabedd456b6a02fe5
Christophe <contact@christophe-ribot.fr>
2015-06-15 19:29:09 +0200

    Added headers

--- Snake/SnakeItem.cpp
+++ Snake/SnakeItem.cpp
@@ -1,3 +1,24 @@
+/* This file is part of AnimUT project.
+ * <https://github.com/christophe-r/UTBM-QtFramework-AnimUT>
+ *
+ * AnimUT project was originally created in spring 2015 by:
+ *  - Vincent MARSEGUERRA <http://vincent.marseguerra.fr/>
+ *  - Christophe RIBOT <http://www.christophe-ribot.fr/>
+ *
+ * AnimUT project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AnimUT project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AnimUT project. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QDebug>
 #include <QMessageBox>
 
